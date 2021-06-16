426dd83f31517dcfd1e53f43e3fc0bafd249b9e3
HectorPat <83776645+HectorPat@users.noreply.github.com>
2021-06-15 22:27:45 -0500

    Create chapter4ex1.c

---
+++ chapter4ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char Nombre[] = "Hector Pat Sosa";
+    char NSS[] = "19180168361";
+    char Cumple[] = "24 de agosto";
+    printf(" Mi nombre es %s \n Mi Numero de Seguridad Social es %s \n Mi cumpleaños es el %s", Nombre, NSS, Cumple);
+
+    return 0;
+}
